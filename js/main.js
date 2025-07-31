abec87c6a27c5974913583f4f5c6cc53af3ff757
parksangseok <park0157@naver.com>
2025-08-01 08:39:05 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -35,5 +35,71 @@ $(function () {
     speed: 1500,
   });
 
+  function initMobileSlick() {
+    if (window.innerWidth <= 767) {
+      if (!$('#newsWrap .slick').hasClass('slick-initialized')) {
+        $("#newsWrap .slick").slick({
+          autoplay: true,
+          arrows: false,
+          dots: true,
+          accessibility: false,
+          draggable: true,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          zIndex: 1000,
+          pauseOnHover: false,
+          autoplaySpeed: 5000,
+          speed: 1500,
+        });
+      }
+
+      if (!$('#reviewWrap .slick').hasClass('slick-initialized')) {
+        $("#reviewWrap .slick").slick({
+          autoplay: true,
+          arrows: false,
+          dots: true,
+          accessibility: false,
+          draggable: true,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          zIndex: 1000,
+          pauseOnHover: false,
+          autoplaySpeed: 7000,
+          speed: 1500,
+        });
+      }
+
+      if (!$('#advantageWrap .slick').hasClass('slick-initialized')) {
+        $("#advantageWrap .slick").slick({
+          autoplay: true,
+          arrows: false,
+          dots: true,
+          accessibility: false,
+          draggable: true,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          zIndex: 1000,
+          pauseOnHover: false,
+          autoplaySpeed: 7000,
+          speed: 1500,
+        });
+      }
+    } else {
+      $('#newsWrap .slick').filter('.slick-initialized').slick('unslick');
+      $('#reviewWrap .slick').filter('.slick-initialized').slick('unslick');
+      $('#advantageWrap .slick').filter('.slick-initialized').slick('unslick');
+    }
+  }
+
+  $(document).ready(function () {
+    initMobileSlick();
+  });
+
+  $(window).on('resize', function () {
+    initMobileSlick();
+  });
 
 });
